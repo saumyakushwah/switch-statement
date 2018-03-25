659bd5f1f2eba13f776d803e2c7ab56490c4649b
Saumya Kushwah <saumyakushwahsk@gmail.com>
2018-03-26 00:01:53 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    switch(c)
+    {case r:
+    cout<<"Red";
+    case b:
+    cout<<"Blue";
+    case p:
+    cout<<"Pink";
+    case w:
+    cout<<"White";
+    default:
+        cout<<"Colorblind";}
+    return 0;
+}
